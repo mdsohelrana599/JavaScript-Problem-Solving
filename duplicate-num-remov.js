76c8bfeda8089b97f283e6eaed7e68592ecc01dc
mdsohelrana599 <mdsohelrana812005@gmail.com>
2026-09-22 19:24:12 +0600

    length-number

---
+++ duplicate-num-remov.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 2, 2, 3, 4, 4, 5, 6, 6, 1];
+const uniqueNumbers = [...new Set(numbers)];
+
+console.log(uniqueNumbers);
---
+++ length-number.js
@@ -0,0 +1,3 @@
+const numbers= [1,2,2,3,3,3,3,5,6,4]
+const count = numbers.filter(num => num === 3).length
+console.log(count)
