67097da15e0333234af17e8bf775dfffe9e6cc5b
mdsohelrana599 <mdsohelrana812005@gmail.com>
2026-09-22 19:00:54 +0600

    sum

---
+++ max-number.js
@@ -0,0 +1,3 @@
+const number = [10, 25, 5, 30, 40, 15];
+const max = Math.max(...number);
+console.log(max);
---
+++ sum-number.js
@@ -0,0 +1,6 @@
+const numbers = [10, 20, 30, 40];
+const total = numbers.reduce((sum, num) => {
+  return sum + num;
+}, 0);
+
+console.log(total);
